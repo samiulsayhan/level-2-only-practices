20e7edda5d1e392393626e3ddf983c0661cea628
samiulsayhan <samiulsayhan@gmail.com>
2026-01-10 00:13:50 +0600

    new

---
+++ M 2/module 6/m-6-3.ts
@@ -0,0 +1,16 @@
+// dynamicaly generalize
+
+
+type genericArray = Array<string>
+
+// const friend : string[] =["a","b","c","d"]
+// const friend : Array<string> =["a","b","c","d"]
+const friend : genericArray =["a","b","c","d"]
+
+
+// const rollNumber : number[] = [3,1,2,4]
+const rollNumber : Array<number> = [3,1,2,4]
+
+// const isEligaleList : boolean[]=[true,false,true]
+const isEligaleList : Array<boolean>=[true,false,true]
+
