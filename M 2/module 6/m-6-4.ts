d36b7e578b24551c391f5d9b098ba6f039e912e3
samiulsayhan <samiulsayhan@gmail.com>
2026-01-11 05:26:24 +0600

    first of the day

---
+++ M 2/module 6/m-6-4.ts
@@ -0,0 +1,49 @@
+// generic With Interface
+
+interface Developer<i> {
+    name: string,
+    salary : number,
+    device:{
+         brand :string,
+         model:string,
+         relesedDate: string
+    }
+    smartWatch: i
+
+}
+
+const poorDeveloper : Developer<{
+    heartRate: string,
+    stopWatch: boolean
+}> ={
+    name: "poorMan",
+    salary: 30000,
+    device:{
+        brand: "bmw",
+        model: "8i",
+        relesedDate: "2020/05/24"
+    },
+    smartWatch:{
+        heartRate:"200",
+        stopWatch: true
+    }
+}
+// 
+
+const richDeveloper : Developer<{
+    heartRate: string,
+    CallSupport: boolean,
+    
+}> ={
+    name: "poorMan",
+    salary: 30000,
+    device:{
+        brand: "bmw",
+        model: "8i",
+        relesedDate: "2020/05/24"
+    },
+    smartWatch:{
+        heartRate:"200",
+        CallSupport:true
+    }
+}
