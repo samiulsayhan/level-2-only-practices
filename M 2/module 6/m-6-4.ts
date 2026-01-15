d683de9da99309ad9881aed908095991f1d84ff1
samiulsayhan <samiulsayhan@gmail.com>
2026-01-16 02:07:48 +0600

    dynamic generic details

--- M 2/module 6/m-6-4.ts
+++ M 2/module 6/m-6-4.ts
@@ -1,6 +1,9 @@
 // generic With Interface
 
-interface Developer<i> {
+interface Developer<i,x=null> { 
+// extra paramiter ..x.. nisi but kaj same y ar moto recive kore ar paramiter
+// x=null dear karon jodi karo bike na thake taele value 0 hobe , tae x ar default value null disi mane <x=null>
+
     name: string,
     salary : number,
     device:{
@@ -8,9 +11,26 @@ interface Developer<i> {
          model:string,
          relesedDate: string
     }
-    smartWatch: i
+    smartWatch: i,
+    bike?: x 
 
 }
+/**
+uporer interface a amra deksi Delveloper<i> aetae <i> recevier ar moto kaj kore ,
+ar mane holo nicher line ar aetae dea data 
+.......
+const poorDeveloper : Developer<{
+    heartRate: string,
+    stopWatch: boolean
+}>
+.......
+ ae data uporer interface ... <i>.... ar vitor jabe karon <i> aeta auto recive korbe
+ then interface ardynamic generic ar neom onujae ae Developer<i> ar data .....smartWatch : i .... aetar vitor jabe
+ then sob gula variable type check hone normali temon boro kisu na 
+
+ */
+
+//  watch this video for clarification:-  https://app.usebubbles.com/tk9CuFYnHoMiRUC3BeWf6T
 
 const poorDeveloper : Developer<{
     heartRate: string,
@@ -29,14 +49,19 @@ const poorDeveloper : Developer<{
     }
 }
 // 
-
-const richDeveloper : Developer<{
+type richDevelopoerExtraTypes = {
     heartRate: string,
     CallSupport: boolean,
     calculator: boolean,
     AiSupport: boolean
     
-}> ={
+}
+
+const richDeveloper : Developer< richDevelopoerExtraTypes > ={
+// aekhane o amra poorDeveloper ar moto notun object type nite partam kintu code 
+// code clean rakhar joono ae new object gula upore arekta notun interface banie tar vitor nici
+// ar ae Developer<> ar vitor oi interface ar name ta dea disi ,, simple(0`0) ar code o clean holo
+
     name: "richMan",
     salary: 30000,
     device:{
@@ -53,5 +78,4 @@ const richDeveloper : Developer<{
 }
 
 
-console.log();
 
