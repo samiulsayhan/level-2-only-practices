e270a0d68782e5934b032bb04070ca391ae68c42
samiulsayhan <samiulsayhan@gmail.com>
2026-01-18 23:34:29 +0600

    new moduel practice and explain

--- M 2/module 6/m-6-6.ts
+++ M 2/module 6/m-6-6.ts
@@ -1,1 +1,31 @@
 //  constraint : strict rules deya
+type mustStudentInfo = {id:number,name:string}
+
+const addToCourse = <T extends mustStudentInfo>(studentInfo:T)=>{
+    return{
+        course: "Next Level",
+        willShowDetails:true,
+        ...studentInfo,
+    }
+}
+
+// watch the video :- https://app.usebubbles.com/htsmEUWuJURH5tMhdK4K11
+
+const student3 = {
+    id:323,
+    name:"dsdw",
+    bike:true
+}
+
+const student4 ={
+    id:434,
+    name:"koi",
+    hasPen: true,
+    isMarried: true
+}
+const student5 ={
+    hasBike:true
+}
+
+const FinalResult = addToCourse(student4)
+console.log(FinalResult);
